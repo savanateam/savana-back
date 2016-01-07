0a5a5b41e3e14cdc95e08ce460c46fbefa620826
recklessromeo <marinoni.dario@gmail.com>
2016-01-07 21:26:01 +0100

    fix import settings

--- settings/common.py
+++ settings/common.py
@@ -16,7 +16,7 @@
 
 import os.path, sys, os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(os.path.abspath(os.path.join(BASE_DIR, '..', 'taiga-back')))
+sys.path.append(os.path.abspath(os.path.join(BASE_DIR, 'taiga-back')))
 
 APPEND_SLASH = False
 ALLOWED_HOSTS = ["*"]
